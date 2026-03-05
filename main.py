6e51c29d43884bacc650ecdb6f199d962edba9bd
Yevheniia <missisvovk@gmail.com>
2026-03-05 10:09:41 +0200

    final code update with root and os

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#Як запустити FastAPI в контейнері Docker: https://www.youtube.com/watch?v=YQbEx5fS39M&list=PLeLN0qH0-mCVQKZ8-W1LhxDcVlWtTALCS&index=13
-
 from fastapi import FastAPI
 import os
 import uvicorn
@@ -16,5 +14,4 @@ async def get_users():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000)) 
-    uvicorn.run("main:app", host="0.0.0.0", port=port) #uvicorn.run - це практика, щоб запустити додаток(цей код) просто через термінал python main.py
-#"main:app" це посилання до app = FastAPI(). І якщо раніше ми були на локальному 127.0.0.. то тепер в докер треба це прописати 
+    uvicorn.run("main:app", host="0.0.0.0", port=port) 
